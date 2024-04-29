552fe6875a38687e01e9f8197d9c93f541421f29
kabalport <kabalport@gmail.com>
2024-04-30 01:25:54 +0900

    init

--- src/test/java/com/example/tddecommerce/member/concurrency/isolation/TransactionIsolationTest.java
+++ src/test/java/com/example/tddecommerce/member/concurrency/isolation/TransactionIsolationTest.java
@@ -2,10 +2,7 @@ package com.example.tddecommerce.member.concurrency.isolation;
 
 import com.example.tddecommerce.IntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.annotation.Isolation;
 
